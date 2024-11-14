ddfd878d05d5da381d644f7364a32f7e4d67e8d0
AverageJavaEnj0yer <reservaccaynt@gmail.com>
2024-11-14 14:50:02 +0300

    Небольшой фикс

--- app/src/main/java/com/example/hardwareapp/AddProductScreen.kt
+++ app/src/main/java/com/example/hardwareapp/AddProductScreen.kt
@@ -2,6 +2,8 @@ package com.example.hardwareapp
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -9,7 +11,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import com.example.hardwareapp.data.Product
 import com.example.hardwareapp.data.ProductDao
@@ -21,7 +22,7 @@ fun AddProductScreen(onProductAdded: () -> Unit, onBackClick: () -> Unit, produc
     var name by remember { mutableStateOf("") }
     var price by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
-    var imageUrl by remember { mutableStateOf("") } // Новое поле для ссылки на изображение
+    var imageUrl by remember { mutableStateOf("") }
     var errorMessage by remember { mutableStateOf("") }
     val coroutineScope = rememberCoroutineScope()
 
@@ -40,108 +41,110 @@ fun AddProductScreen(onProductAdded: () -> Unit, onBackClick: () -> Unit, produc
     var expanded by remember { mutableStateOf(false) }
     var selectedCategory by remember { mutableStateOf(categories[0]) }
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        IconButton(onClick = onBackClick) {
-            Icon(
-                imageVector = Icons.Filled.ArrowBack,
-                contentDescription = null
-            )
-        }
-
-        Text("Добавить товар", style = MaterialTheme.typography.headlineMedium)
-
-        // Выпадающий список для выбора категории
-        Box(modifier = Modifier.fillMaxWidth()) {
-            OutlinedTextField(
-                value = selectedCategory,
-                onValueChange = { },
-                readOnly = true,
-                label = { Text("Категория") },
-                trailingIcon = {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowDropDown,
-                        contentDescription = null,
-                        modifier = Modifier.clickable { expanded = !expanded }
-                    )
-                },
-                modifier = Modifier.fillMaxWidth()
-            )
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                categories.forEach { category ->
-                    DropdownMenuItem(
-                        text = { Text(category) },
-                        onClick = {
-                            selectedCategory = category
-                            expanded = false
-                        }
-                    )
-                }
+        item {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = null
+                )
             }
-        }
 
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Название") }
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
-            value = price,
-            onValueChange = { price = it },
-            label = { Text("Цена") }
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
-            value = description,
-            onValueChange = { description = it },
-            label = { Text("Описание") }
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
-            value = imageUrl,
-            onValueChange = { imageUrl = it },
-            label = { Text("Ссылка на изображение") }
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+            Text("Добавить товар", style = MaterialTheme.typography.headlineMedium)
 
-        Button(onClick = {
-            if (selectedCategory.isEmpty() || name.isEmpty() || price.isEmpty() || description.isEmpty() || imageUrl.isEmpty()) {
-                errorMessage = "Пожалуйста, заполните все поля"
-            } else {
-                coroutineScope.launch {
-                    try {
-                        val product = Product(
-                            category = selectedCategory,
-                            name = name,
-                            price = price.toDouble(),
-                            description = description,
-                            imageUrl = imageUrl // Добавление ссылки на изображение
+            // Выпадающий список для выбора категории
+            Box(modifier = Modifier.fillMaxWidth()) {
+                OutlinedTextField(
+                    value = selectedCategory,
+                    onValueChange = { },
+                    readOnly = true,
+                    label = { Text("Категория") },
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowDropDown,
+                            contentDescription = null,
+                            modifier = Modifier.clickable { expanded = !expanded }
                         )
-                        productDao.insert(product)
-                        onProductAdded()
-                    } catch (e: Exception) {
-                        errorMessage = "Ошибка при добавлении товара: ${e.message}"
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    categories.forEach { category ->
+                        DropdownMenuItem(
+                            text = { Text(category) },
+                            onClick = {
+                                selectedCategory = category
+                                expanded = false
+                            }
+                        )
                     }
                 }
             }
-        }) {
-            Text("Добавить товар")
-        }
 
-        if (errorMessage.isNotEmpty()) {
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = errorMessage, color = MaterialTheme.colorScheme.error)
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Название") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedTextField(
+                value = price,
+                onValueChange = { price = it },
+                label = { Text("Цена") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedTextField(
+                value = description,
+                onValueChange = { description = it },
+                label = { Text("Описание") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedTextField(
+                value = imageUrl,
+                onValueChange = { imageUrl = it },
+                label = { Text("Ссылка на изображение") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                if (selectedCategory.isEmpty() || name.isEmpty() || price.isEmpty() || description.isEmpty() || imageUrl.isEmpty()) {
+                    errorMessage = "Пожалуйста, заполните все поля"
+                } else {
+                    coroutineScope.launch {
+                        try {
+                            val product = Product(
+                                category = selectedCategory,
+                                name = name,
+                                price = price.toDouble(),
+                                description = description,
+                                imageUrl = imageUrl
+                            )
+                            productDao.insert(product)
+                            onProductAdded()
+                        } catch (e: Exception) {
+                            errorMessage = "Ошибка при добавлении товара: ${e.message}"
+                        }
+                    }
+                }
+            }) {
+                Text("Добавить товар")
+            }
+
+            if (errorMessage.isNotEmpty()) {
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = errorMessage, color = MaterialTheme.colorScheme.error)
+            }
         }
     }
 }
